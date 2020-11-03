d54c96417e77288df37f25dc962ff928199a23af
Rachel Peterson <rachelp5446@gmail.com>
2020-11-03 10:56:51 -0600

    second commit

--- src/components/DisplayTabs.jsx
+++ src/components/DisplayTabs.jsx
@@ -3,10 +3,8 @@ const DisplayTabs = props => {
 
     const { idx, name, isSelected, toggleTab} = props;
 
-    return <div onClick={() => toggleTab(idx)} className="col-3 border rounded text-center mr-3 p-3" style={isSelected ? {backgroundColor:"darkseagreen"} : {}}>
-            <div className="bg-transparent">
+    return <div onClick={() => toggleTab(idx)} className="zoom col-3 border rounded text-center mr-4 mb-3 p-3" style={isSelected ? {backgroundColor:"darkseagreen"} : {}}>
                 <h3 className="bg-transparent">{ name }</h3>
-            </div>
         </div>
 
 }
